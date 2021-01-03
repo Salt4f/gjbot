802a21f6b4da29c2d80aa83efd02b73872ec8459
Salt4f <47352634+Salt4f@users.noreply.github.com>
2021-01-03 12:46:52 +0100

    Update tables.sql
    Added tickets table

--- tables.sql
+++ tables.sql
@@ -3,6 +3,10 @@ CREATE TABLE inscrits (
     username    text NOT NULL CHECK (username <> '')
 );
 
+CREATE TABLE tickets (
+    id          text PRIMARY KEY
+);
+
 CREATE TABLE grups (
     grup_name   text PRIMARY KEY
 );
@@ -11,12 +15,15 @@ CREATE TABLE participants (
     email       text PRIMARY KEY,
     discord_id  text NOT NULL UNIQUE CHECK (discord_id <> ''),
     discord_tag text NOT NULL UNIQUE CHECK (discord_tag <> ''),
+    ticket      text NOT NULL UNIQUE,
     grup        text DEFAULT NULL,
     
     FOREIGN KEY(email) references inscrits(email),
+    FOREIGN KEY(ticket) references tickets(id),
     FOREIGN KEY(grup) references grups(grup_name)
 );
 
 
 INSERT INTO inscrits VALUES('example@mail.com', 'pepito');
+INSERT INTO tickets VALUES('1234');
 --INSERT INTO participants VALUES('Pepito#0000');
