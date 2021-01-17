b419bd56b86c439572f08109cdd3d6d662b8a504
Salt4f <Salt4f@github.com>
2021-01-17 14:19:51 +0100

    Fixed add command

--- app/commands/add.js
+++ app/commands/add.js
@@ -51,20 +51,5 @@ module.exports = {
             })
         });
 
-        
-
-        console.log(`${msg.author.id} (${msg.author.tag}) intenta ejecutar gjbot group join`);
-        
-        var pool = new Database.Pool();
-
-        pool.query("UPDATE participants SET grup = $1::text WHERE discord_id = $2::text", [args[1], msg.author.id])
-        .then(result => {
-            console.log(`${msg.author.id} (${msg.author.tag}) se une al grupo ${args[1]}`);
-            msg.reply(`¡Unido al grupo ${args[1]}!`);
-        }).catch(err => {
-            if (err.code == '23503') msg.reply(`El grupo ${args[1]} no existe`)
-            else console.error(err);
-        });
-
 	},
 };
