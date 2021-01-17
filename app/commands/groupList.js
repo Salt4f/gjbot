2b50a0fdb9590020ded97d685ef6f77c380fb2b9
fonserbc <fonserbc@gmail.com>
2021-01-17 18:48:12 +0100

    Fixes #4

--- app/commands/groupList.js
+++ app/commands/groupList.js
@@ -3,7 +3,7 @@ const Database = require("pg");
 
 module.exports = {
 	name: 'group list',
-	description: 'Muestra los grupos creados y sus miembros',
+	description: 'Mostra els grups creats i els seus membres --- Muestra los grupos creados y sus miembros',
 	usage: 'group list',
     /**
      * 
